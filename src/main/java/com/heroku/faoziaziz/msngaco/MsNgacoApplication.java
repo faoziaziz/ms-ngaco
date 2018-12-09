19dced67a888ee891863fced8a6898ff8b5ebd64
azizfaozi <admin@labseni.com>
2018-12-09 20:38:39 +0700

    Percobaan pertama

---
+++ src/main/java/com/heroku/faoziaziz/msngaco/MsNgacoApplication.java
@@ -0,0 +1,12 @@
+package com.heroku.faoziaziz.msngaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNgacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNgacoApplication.class, args);
+	}
+}
